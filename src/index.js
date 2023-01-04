4ff677b07b67cc084efcdeae9def8fb79270c906
Caleb Bush <calebmatthewbush@gmail.com>
2023-01-03 23:26:48 -0600

    Add method for gameboard objects to receive attacks

--- src/index.js
+++ src/index.js
@@ -2,16 +2,44 @@
 const shipProto = {
     hit(){
         this.hits += 1
+        this.isSunk()
     },
     isSunk(){
-        return this.hits === this.length
+        if(this.hits === this.length){
+            this.sunk = true
+        }
     }
 }
 
 
 const gameboardProto = {
-    placeShip(ship, coords){
-        this.ships.push({ship,coords})
+    placeShip(ship, ...coords){
+        this.ships.push({ship, coords})
+        
+    },
+    receiveAttack(attack){
+        //iterate through gameboard's ships property and check coords of each ship against attack
+        for(let i = 0; i < this.ships.length; i++){
+            for(let j = 0; j < this.ships[i].coords.length; j++){
+                if (this.ships[i].coords[j].toString() == attack.toString()){
+                    this.ships[i].ship.hit()
+                    this.checkSunkShips()
+                    return
+                }
+            }
+        }
+        this.misses.push(attack)
+    },
+    checkSunkShips(){
+        let sunkShipsCount = 0
+        for (let i = 0; i < this.ships.length; i++){
+            if (this.ships[i].ship.sunk == true){
+                sunkShipsCount += 1
+            }
+        }
+        if(sunkShipsCount === this.ships.length){
+            this.allShipsSunk = true
+        }
     }
 }
 
@@ -21,11 +49,15 @@ export const Ship = (length) => {
     let ship = Object.create(shipProto)
     ship.length = length
     ship.hits = 0
+    ship.sunk = false
     return ship
 }
 
 export const Gameboard = () => {
     let board = Object.create(gameboardProto)
+    board.shipCoords = []
     board.ships = []
+    board.misses = []
+    board.allShipsSunk = false
     return board
 }
--- src/test.js
+++ src/test.js
@@ -11,17 +11,53 @@ test("Check whether the ship is sunk or not", ()=> {
     titanic.hit()
     titanic.hit()
     titanic.hit()
-    expect(titanic.isSunk()).toBe(true)
+    expect(titanic.sunk).toBe(true)
 })
 
 test("Factory function to create a ship return the correct length", () =>{
     expect(Ship(7).length).toBe(7)
 })
 
-test("Check whether Gameboard factory can place a ship", ()=>{
+test("Check whether Gameboard factory can place a ship at a location", ()=>{
     let redSea = Gameboard()
     let edmondFitzgerald = Ship(4)
-    redSea.placeShip(edmondFitzgerald, [1,2])
+    redSea.placeShip(edmondFitzgerald, [1,2],[2,2],[3,2],[4,2])
     expect(redSea.ships.length).toBe(1)
 })
 
+test("determine if receiveAttack() successfully calls hit() function on ship", () =>{
+    let redSea = Gameboard()
+    let edmondFitzgerald = Ship(4)
+    redSea.placeShip(edmondFitzgerald, [1,2],[2,2],[3,2],[4,2])
+    redSea.receiveAttack([1,2])
+    expect(edmondFitzgerald.hits).toBe(1)
+})
+
+test("determine if misses are correctly stored in gameboard object", ()=>{
+    let redSea = Gameboard()
+    let edmondFitzgerald = Ship(4)
+    redSea.placeShip(edmondFitzgerald, [1,2],[2,2],[3,2],[4,2])
+    redSea.receiveAttack([7,0])
+    expect(redSea.misses[0]).toEqual([7,0])
+})
+
+test("Check if not all ships on a gameboard are sunk", ()=> {
+    let bikiniBottom = Gameboard()
+    let canoe = Ship(1)
+    let yacht = Ship(1)
+    bikiniBottom.placeShip(canoe, [3,3])
+    bikiniBottom.placeShip(yacht, [1,1])
+    bikiniBottom.receiveAttack([3,3])
+    expect(bikiniBottom.allShipsSunk).toBe(false)
+})
+
+test("Check if all ships on a gameboard are sunk", ()=> {
+    let bikiniBottom = Gameboard()
+    let canoe = Ship(1)
+    let yacht = Ship(1)
+    bikiniBottom.placeShip(canoe, [3,3])
+    bikiniBottom.placeShip(yacht, [1,1])
+    bikiniBottom.receiveAttack([3,3])
+    bikiniBottom.receiveAttack([1,1])
+    expect(bikiniBottom.allShipsSunk).toBe(true)
+})
